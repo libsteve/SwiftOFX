4bb43cb1c337031790ef85b40316ce250688cd34
Steven Brunwasser <sbrunwasser@gmail.com>
2017-02-27 21:18:24 -0800

    Initial Commit

---
+++ SwiftOFX/SwiftOFX.h
@@ -0,0 +1,19 @@
+//
+//  SwiftOFX.h
+//  SwiftOFX
+//
+//  Created by Steve Brunwasser on 2/27/17.
+//  Copyright © 2017 Steve Brunwasser. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftOFX.
+FOUNDATION_EXPORT double SwiftOFXVersionNumber;
+
+//! Project version string for SwiftOFX.
+FOUNDATION_EXPORT const unsigned char SwiftOFXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftOFX/PublicHeader.h>
+
+
